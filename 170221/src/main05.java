f44fb52bba98e8013de0ebe2c72c91205c5c53a2
jamess90 <jamess90@naver.com>
2017-02-21 13:29:14 +0900

    study at 170221

--- 170221/src/main05.java
+++ 170221/src/main05.java
@@ -1,11 +1,19 @@
 
 public class main05 {
 	public static void main(String[] args) {
-		
-		for(int i = 9; i > 1; i--){
-			for(int j = 9; j > 0; j--){
-				System.out.println(i+"x"+j+"="+ i*j);
+		int num = 2;
+		int vertical = 4;
+		int dan = 20;
+		for (int k = 0; k < dan/vertical; k++) {
+			for (int i = 1; i < 10; i++) {
+				for (int j = num; j < num + vertical; j++) {
+					if (j < dan + 1)
+						System.out.print(j + " x " + i + " = " + i * j + "	");
+				}
+				System.out.println();
 			}
+			num += vertical;
+			System.out.println();
 		}
 	}
 }
